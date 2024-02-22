7f65cc5a5ecce9a4ffc3fcc61fcd9a8ef0ccb364
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-02-22 13:09:25 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { ReactTyped } from "react-typed";
+import NavBar from "./components/NavBar";
+import styles from "./App.module.css";
+import background from "./background.jpg";
 function App() {
+  const [shrinkTitle, setShrinkTitle] = useState(false);
+  const [on, setOn] = useState(true);
+  const timeToShrink = 5000;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShrinkTitle(true);
+    }, timeToShrink);
+
+    return () => clearTimeout(timer);
+  }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOn(false);
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const shouldScrolling = scrollTop > 0;
+
+      if (shouldScrolling !== scrolling) {
+        setScrolling(shouldScrolling);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolling]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      {on && (
+        <main className={styles.container}>
+          <div className={styles.textOverlay}>
+            <ReactTyped
+              className={styles.title}
+              strings={["STAR    WARS    PLANETS"]}
+              typeSpeed={155}
+            />
+          </div>
+        </main>
+      )}
+      {!on && (
+        <div
+          className={`fixed bg-white rounded-xl shadow-md box-border h-16 min-w-[90vw] top-6 left-14 transition-all ${
+            scrolling
+              ? "opacity-60 transform -translate-y-10"
+              : "opacity-100 transform translate-y-0"
+          }`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <NavBar />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between ">
+        <div className="flex items-center ml-10">
+          <div className="btn btn-ghost text-2xl text-black font-extrabold font-sans">
+            STAR WARS
+          </div>
+        </div>
+        <div className="flex m-1">
+          <div className="btn btn-ghost m-1 font-bold text-black text-xl rounded-full">
+            Look For Planets
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
